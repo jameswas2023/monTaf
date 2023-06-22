37ff3196d103bdcb9c5803b462e2628b6b48b3f4
jameswas2023 <135992394+jameswas2023@users.noreply.github.com>
2023-06-22 16:37:03 +0200

    Add files via upload

---
+++ CourseAuto/CourseAuto/Program.cs
@@ -0,0 +1,11 @@
+﻿using CourseAuto;
+
+static void Main(string[] args)
+{
+    Voiture voiture1 = new Voiture("Ferrari", 185 , 200 );
+
+
+
+
+
+}
---
+++ CourseAuto/CourseAuto/Voiture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseAuto
+{
+    public class Voiture
+    {
+
+        public string Marque { get; set; }
+        public decimal VitMin { get; set; }
+        public decimal VitMax { get; set; }
+        
+        public Voiture(string marque, int vitMin, int vitMax) 
+        {
+
+            Marque = marque;
+            VitMin = vitMin;
+            VitMax = vitMax;
+
+        }
+
+        
+    }
+}
